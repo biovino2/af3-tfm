8e9526ef48e3d497795875afba583388c6d16ebf
Benjamin Iovino <benjamin.iovino@czbiohub.org>
2024-12-14 09:24:55 -0800

    cpu/gpu handling

--- src/run_af3.py
+++ src/run_af3.py
@@ -26,12 +26,74 @@ def get_config(config_file: str, **kwargs) -> dict:
 
     # Update config based on kwargs
     for key, value in kwargs.items():
-        if key == "data_dir":
-            config["slurm_output"] = f"{value}/slurm-%j.log"
-            config["slurm_error"] = f"{value}/slurm-%j.log"
-            config["output_dir"] = value
-        else:
-            config[key] = value
+        config[key] = value
+
+    return config
+
+
+def cpu_config(path: str, job: str) -> dict:
+    """
+    Returns config for a CPU job.
+
+    Args:
+        path (str): The path to the JSON file.
+        job (str): The job name.
+
+    Returns:
+        dict: The configuration dictionary.
+    """
+
+    config = get_config(
+            "src/config.yaml",
+            output_dir=f"{path}",
+            input_json=f"{path}/{job}/{job}.json",
+            mail_user="benjamin.iovino@czbiohub.org",
+            slurm_output=f"{path}/{job}/slurm-%j.log",
+            slurm_error=f"{path}/{job}/slurm-%j.log",
+            partition="cpu",
+            run_inference=False,
+            time="3:00:00",
+        )
+
+    return config
+
+
+def gpu_config(path: str, job: str, constraint: str = "None") -> dict:
+    """
+    Returns config for a GPU job.
+
+    Args:
+        path (str): The path to the JSON file.
+        job (str): The job name.
+        constraint (str): The GPU constraint.
+
+    Returns:
+        dict: The configuration dictionary.
+    """
+
+    # Check for _data json file
+    if os.path.exists(f"{path}/{job}/{job}_data.json"):
+        input_json = f"{path}/{job}/{job}_data.json"
+        run_data_pipeline = False
+    else:
+        input_json = f"{path}/{job}/{job}.json"
+        run_data_pipeline = True
+
+    config = get_config(
+            "src/config.yaml",
+            output_dir=f"{path}",
+            input_json=input_json,
+            mail_user="benjamin.iovino@czbiohub.org",
+            slurm_output=f"{path}/{job}/slurm-%j.log",
+            slurm_error=f"{path}/{job}/slurm-%j.log",
+            partition="gpu",
+            run_data_pipeline=run_data_pipeline,
+            time="3:00:00"
+        )
+    
+    # Specific GPU options
+    if constraint != "None":
+        config["constraint"] = constraint
 
     return config
 
@@ -62,9 +124,9 @@ def generate_slurm_script(config: dict[str]) -> str:
     
     # GPU specific options
     if config["partition"] == "gpu":
-        slurm_script += f"#SBATCH --gpus={config["gpus"]}\n"
+        slurm_script += f"#SBATCH --gpus={config['gpus']}\n"
     if config["constraint"] != "None":
-        slurm_script += f"#SBATCH --constraint={config["constraint"]}\n"
+        slurm_script += f"#SBATCH --constraint={config['constraint']}\n"
 
     slurm_script += f"""
 module load alphafold/3.0.0
@@ -86,24 +148,21 @@ def main():
     """
     """
 
+    partition = 'gpu'
     path = "data/jobs/motifs_two"
     for job in os.listdir(path):
 
-        # Update config with job specific information
-        config = get_config(
-            "src/config.yaml",
-            data_dir=f"{path}/{job}",
-            input_json=f"{path}/{job}/{job}.json",
-            mail_user="benjamin.iovino@czbiohub.org",
-            partition="cpu",
-            run_inference=False,
-        )
+        # Config based on cpu/gpu
+        if partition == 'cpu':
+            config = cpu_config(path, job)
+        if partition == 'gpu':
+            config = gpu_config(path, job)
 
         # Submit slurm script
         slurm_script = generate_slurm_script(config)
-        with open(f"{path}/{job}/run_af3.sh", "w") as f:
+        with open(f"{path}/{job}/run_af3_{partition}.sh", "w") as f:
             f.write(slurm_script)
-        sp.run(["sbatch", f"{path}/{job}/run_af3.sh"])
+        sp.run(["sbatch", f"{path}/{job}/run_af3_{partition}.sh"])
 
 
 if __name__ == "__main__":
